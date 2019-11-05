73bac5858bace65d204ea63a77c4b0044f98cd7b
Ricky Kirkham <rickki@microsoft.com>
2019-11-05 12:26:05 -0800

    fixing code comment about showResults and fixing gitignore

--- src/Office-Add-in-ASPNET-SSO-WebAPI/Scripts/HomeES6.js
+++ src/Office-Add-in-ASPNET-SSO-WebAPI/Scripts/HomeES6.js
@@ -148,13 +148,8 @@ function handleServerSideErrors(result) {
 // Displays the data, assumed to be an array.
 function showResult(data) {
 
-	// Note that in this sample, the data parameter is an array of OneDrive file/folder
-	// names. Encoding/sanitizing to protect against Cross-site scripting (XSS) attacks
-	// is not needed because there are restrictions on what characters can be used in 
-	// OneDrive file and folder names. These restrictions do not necessarily apply 
-	// to other kinds of data including other kinds of Microsoft Graph data. So, to 
-	// make this method safely reusable in other contexts, it uses the jQuery text() 
-	// method which automatically encodes values that are passed to it.
+	// Use jQuery text() method which automatically encodes values that are passed to it,
+    // in order to protect against injection attacks.
 	$.each(data, function (i) {
 		var li = $('<li/>').addClass('ms-ListItem').appendTo($('#file-list'));
 		var outerSpan = $('<span/>').addClass('ms-ListItem-secondaryText').appendTo(li);
